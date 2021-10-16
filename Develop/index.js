f95319801632712776472995188c8d6ada8694ee
Anusha <anushaselvan@gmail.com>
2021-10-16 18:41:51 +1100

    Added prompts to get info for README

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function questions() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Project Title:',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Description:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Installation:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Usage:',
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Credits:',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'License:',
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'Features:',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How to Contribute:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Tests:',
+        },
+    ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+   
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log('Enter the following details to create a README.md file for your project');
+    questions()
+
+    .then((answers) => fs.writeFileSync('README.md', generateMarkdown(answers)))
+    .then(() => console.log('Successfully created README.md'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
